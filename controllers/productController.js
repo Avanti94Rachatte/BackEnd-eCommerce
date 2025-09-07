58cc9b6c8a48bbc3aa67f115442d75e8b1505136
Avanti <Avanti94Rachatte@gmail.com>
2025-09-07 13:02:50 +0530

    modify 4 backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,13 +23,18 @@ exports.getProducts = async (req, res) => {
 
 exports.getProductsByCategory = async (req, res) => {
   try {
-    const category = req.params.category;
-    const products = await Product.find({ category });
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+      const category = req.params.category;
+      const products = await Product.find({ category: category });
+  
+      if (products.length === 0) {
+        return res.status(404).json({ message: 'No products found in this category' });
+      }
+  
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error: error.message });
+    }
+  };
 
 
 // Get single product
